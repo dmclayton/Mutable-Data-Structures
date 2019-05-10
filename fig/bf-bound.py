5074ca356f9ce6b9a2b247617549cfb9b293ec19
Christopher Patton <chrispatton@gmail.com>
2019-05-10 15:28:07 -0400

    WIP: bound viz

--- fig/bf-bound.py
+++ fig/bf-bound.py
@@ -5,21 +5,33 @@ from pylab import *
 # create an array of 256 points (x), from -pi to +pi
 points = 100
 
-q = 1000
-m = 1<<12
-n = 100
-k = 28
-r = 1
-salt = 128
-M = np.linspace(1, m, points)
-
 def Mitz(k, n, m):
     return (1 - np.exp(-1 * float(k*n) / float(m)))**k
 
-Ym = np.cos(M)
+def Us(r, q, k, n, m):
+    p = float(Mitz(k, n, m))
+    x = p * q
+    if r <= x:
+        return 1.0
+    return ((x / r)**r) * np.exp(r - x)
+
+q = 1<<30
+n = 100
+k = 16
+r = 1
+
+start = 1<<12
+end = (1<<16)
+M = np.linspace(start, end, (end-start)/8)
+
+N = range(100,1100,100)
 
 # compute the plot
-plot(M, map(lambda X:Mitz(k,n,X), M))
+lines = []
+for n in N:
+    line, = plot(M, map(lambda X:Us(r,q,k,n,X), M))
+    lines.append(line)
+legend([lines[0], lines[-1]], ['$n=%d$' % N[0], '$n=%d$' % N[-1]])
 
 # Show it in a new window
 savefig('bf-bound.pdf')
